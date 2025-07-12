bc9a6280ead0c923f012e042cf4cc27d5632e8c9
Rooh2os <117789258+Rooh2os@users.noreply.github.com>
2025-07-12 13:49:55 -0500

    Started user input

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import random,string
+import basic,random,string
+print("WARNING: This program saves passwords for your use. The passwords are stored in plain text. USE WITH CAUTION")
 
-pw = ""
-chrs = string.ascii_letters + string.digits + string.punctuation
-while len(pw) != int(input("lenth of password")):
-    pw += random.choice(chrs)
-print(pw)
+input = None
+while input == None:
+    try:
+        input = int(input(""))
+    except(ValueError,IndexError):
+            basic.clear
+            print("Oops, thats not a valid choice. Try again.\n")
+
+if input == 1:
+    pw = ""
+    chrs = string.ascii_letters + string.digits + string.punctuation
+    while len(pw) != int(input("lenth of password")):
+        pw += random.choice(chrs)
+    print(pw)
